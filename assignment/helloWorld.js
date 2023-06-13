031dff77a669bba4481258fe10d361e856b72b33
Alexa Fleming <alexaflemingmn@gmail.com>
2023-06-13 16:54:51 -0500

    unit 1 homework

---
+++ assignment/helloWorld.js
@@ -0,0 +1,13 @@
+console.log("hello");
+
+let x = 1;
+
+let game = "zelda";
+let title= "tolk";
+
+game = game + ' ' + title;
+console.log('game' , game);
+
+let isCloudy = false
+
+let toReplace = document.querySelector('#fun')
